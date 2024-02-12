677b716ce5aebb48061f6f1fa247a0b71494e20a
Martonnvok <martonsom@gmail.com>
2024-02-12 13:50:14 +0100

    4 feladat megcsinálása

--- src/modell/Tabla.java
+++ src/modell/Tabla.java
@@ -7,7 +7,7 @@ public class Tabla {
     private char UresCella;
 
     public Tabla(char uresCella) {
-        this.T = new char[8][8];
+         this.T = new char[8][8];
         this.UresCella = uresCella;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -26,61 +26,34 @@ public class Tabla {
     }
 
     public void Elhelyez(int n) {
-        Random random = new Random();
-        for (int i = 0; i < n; i++) {
-            int x = random.nextInt(8);
-            int y = random.nextInt(8);
-            if (T[x][y] == UresCella) {
-                T[x][y] = 'K';
-            } else {
-                i--;
-            }
-        }
+
     }
 
     public boolean UresOszlop(int oszlop) {
-        for (int i = 0; i < 8; i++) {
-            if (T[i][oszlop] == 'K') {
-                return false;
-            }
-        }
+        
         return true;
     }
 
     public boolean UresSor(int sor) {
-        for (int i = 0; i < 8; i++) {
-            if (T[sor][i] == 'K') {
-                return false;
-            }
-        }
+
         return true;
     }
 
     public int getUresOszlopokSzama() {
-        int db = 8;
-        for (int i = 0; i < 8; i++) {
-            if (UresOszlop(i)) {
-                db--;
-            }
-        }
-        return db;
+
+        return 0;
     }
 
     public int getUresSorokSzama() {
-        int db = 8;
-        for (int i = 0; i < 8; i++) {
-            if (UresSor(i)) {
-                db--;
-            }
-        }
-        return db;
+
+         return 0;
     }
 
     public void megjelenitOsz() {
-        System.out.println("Üres oszlopok száma: "+getUresOszlopokSzama());
+
     }
 
     public void megjelenitSor() {
-        System.out.println("Üres sorok száma: "+getUresSorokSzama());
+
     }
 }
